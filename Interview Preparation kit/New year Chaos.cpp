11463604c4b42e15008d5d5e3deb556c8fe762a1
Pradipta Choudhury <72348164+pradipta18choudhury@users.noreply.github.com>
2021-06-25 23:03:44 +0530

    Create New year Chaos.cpp

---
+++ Interview Preparation kit/New year Chaos.cpp
@@ -0,0 +1,165 @@
+/****
+It is New Year's Day and people are in line for the Wonderland rollercoaster ride. Each person wears a sticker indicating their initial position in the queue from  to . Any person can bribe the person directly in front of them to swap positions, but they still wear their original sticker. One person can bribe at most two others.
+
+Determine the minimum number of bribes that took place to get to a given queue order. Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
+
+Example
+
+
+If person  bribes person , the queue will look like this: . Only  bribe is required. Print 1.
+
+
+Person  had to bribe  people to get to the current position. Print Too chaotic.
+
+Function Description
+
+Complete the function minimumBribes in the editor below.
+
+minimumBribes has the following parameter(s):
+
+int q[n]: the positions of the people after all bribes
+Returns
+
+No value is returned. Print the minimum number of bribes necessary or Too chaotic if someone has bribed more than  people.
+Input Format
+
+The first line contains an integer , the number of test cases.
+
+Each of the next  pairs of lines are as follows:
+- The first line contains an integer , the number of people in the queue
+- The second line has  space-separated integers describing the final state of the queue.
+
+Constraints
+
+Subtasks
+
+For  score 
+For  score 
+
+Sample Input
+
+STDIN       Function
+-----       --------
+2           t = 2
+5           n = 5
+2 1 5 3 4   q = [2, 1, 5, 3, 4]
+5           n = 5
+2 5 1 3 4   q = [2, 5, 1, 3, 4]
+Sample Output
+
+3
+Too chaotic
+Explanation
+
+Test Case 1
+
+The initial state:
+
+pic1(1).png
+
+After person  moves one position ahead by bribing person :
+
+pic2.png
+
+Now person  moves another position ahead by bribing person :
+
+pic3.png
+
+And person  moves one position ahead by bribing person :
+
+pic5.png
+
+So the final state is  after three bribing operations.
+
+Test Case 2
+
+No person can bribe more than two people, yet it appears person  has done so. It is not possible to achieve the input state.
+****/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the minimumBribes function below.
+void minimumBribes(vector<int> q) {
+int temp=0;
+for(int i=q.size()-1;i>=0;i--)
+{
+    if(q[i]-(i+1)>2){
+    cout<<"Too chaotic"<<endl;
+    return;
+    }
+    else
+    {
+        for(int j=max(0,q[i]-2);j<i;j++)
+        {
+            if(q[j]>q[i])
+            temp++;
+        }
+    }
+}
+cout<<temp<<endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        string q_temp_temp;
+        getline(cin, q_temp_temp);
+
+        vector<string> q_temp = split_string(q_temp_temp);
+
+        vector<int> q(n);
+
+        for (int i = 0; i < n; i++) {
+            int q_item = stoi(q_temp[i]);
+
+            q[i] = q_item;
+        }
+
+        minimumBribes(q);
+    }
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
+
